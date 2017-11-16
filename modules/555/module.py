eb8775f4d3ea8ee11a423f3a44f23dc281d4e941
cjgriscom <cjgriscom@gmail.com>
2017-11-16 03:23:08 -0500

    Fix overconstraint by overwriting incnstnt values

--- modules/555/module.py
+++ modules/555/module.py
@@ -98,8 +98,9 @@ class Module:
                 elif (hasC1 == 0 and hasR1 == 0): C1, R1, ERR = self.solve_C1_R1(DC, R2, HZ)
                 elif (hasC1 == 0 and hasR2 == 0): C1, R2, ERR = self.solve_C1_R2(R1, DC, HZ)
                 elif (hasR1 == 0 and hasR2 == 0): R1, R2, ERR = self.solve_R1_R2(DC, HZ, C1)
-                elif (hasDC == 0): DC = 100*(R1 + R2)/(R1 + 2.*R2)
-                elif (hasHZ == 0): HZ = 1.44/(C1*(R1+2.*R2))
+                elif (hasDC == 0): DC, HZ, ERR = self.solve_DC_HZ(R1, R2, C1)
+                elif (hasC1 == 0): DC, C1, ERR = self.solve_DC_C1(R1, R2, HZ)
+                elif (hasHZ == 0): DC, HZ, ERR = self.solve_DC_HZ(R1, R2, C1)
                 else:
                     tkMessageBox.showinfo("Error", "The system is overconstrained. Remove a value and try again.")
                     ERR = -1
