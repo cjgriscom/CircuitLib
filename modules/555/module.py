6c2fd9f93212d673dd6f96af9b3e188dcee61730
cjgriscom <cjgriscom@gmail.com>
2017-11-16 03:04:15 -0500

    Fix error handling exceptions

--- modules/555/module.py
+++ modules/555/module.py
@@ -135,7 +135,7 @@ class Module:
     def solve_HZ_R2(self, R1,DC,C1): #Complete
         if R1 == 0:
             tkMessageBox.showinfo("Error", "With a 50% duty cycle, R2 and HZ are dependent")
-            return HZ, R2, 0
+            return 0, 0, -1
         else:
             R2=(DC-100.)*R1/(2*(50.-DC))
             HZ=1.44/((R1+2*R2)*C1)
@@ -153,7 +153,7 @@ class Module:
     def solve_C1_R2(self, R1,DC,HZ): #Complete
         if R1 == 0:
             tkMessageBox.showinfo("Error", "With a 50% duty cycle, C1 and R2 are dependent")
-            return HZ, R2, 0
+            return 0, 0, -1
         else:
             R2=(DC-100.)*R1/(2*(50.-DC))
             C1=1.44/(HZ*(R1+2*R2))
